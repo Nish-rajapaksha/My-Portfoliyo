8fef29361efdf18853f2682311094aa4c3882ac0
Nishadi rajapaksha <93418323+Nish-rajapaksha@users.noreply.github.com>
2022-09-27 14:42:26 +0530

    implementation functions

--- assignments/js/calculator/assets/js/script.js
+++ assignments/js/calculator/assets/js/script.js
@@ -8,4 +8,58 @@ $(".clr").click(function () {
     $('#display').val(" ");
 });
 
+$(".sum").click(function () {
+    num1 =$('#display').val();
+    optr="+";
+    $('#display').val(" ");
+
+
+});
+$(".sub").click(function () {
+    num1 =$('#display').val();
+    optr="-";
+    $('#display').val(" ");
+
+
+});
+$(".divd").click(function () {
+    num1 =$('#display').val();
+    optr="/";
+    $('#display').val(" ");
+
+
+});
+$(".mul").click(function () {
+    num1 =$('#display').val();
+    optr="*";
+    $('#display').val(" ");
+
+
+});
+
+$(".fResult").click(function (){
+    num2 =$('#display').val();
+    result();
+
+});
+function result(){
+    var r=null;
+  switch (optr) {
+      case "-" : r = parseFloat(num1) - parseFloat(num2);
+      $('#display').val(r);break;
+
+      case "+" : r = parseFloat(num1) + parseFloat(num2);
+          $('#display').val(r);break;
+
+      case "*" : r = parseFloat(num1) * parseFloat(num2);
+          $('#display').val(r);break;
+
+      case "/" : r = parseFloat(num1) / parseFloat(num2);
+          $('#display').val(r);break;
+
+  }
+
+
+}
+var num1,num2,optr;
 
