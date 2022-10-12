d7539bf458433c1303edf6eb0e4c7cd9212478e1
Nishadi rajapaksha <93418323+Nish-rajapaksha@users.noreply.github.com>
2022-10-12 21:37:40 +0530

    Update SPA

---
+++ assignments/js/SPA/pos/controller/DashBoardController.js
@@ -0,0 +1,62 @@
+$("#homeContent").css("display", "block");
+$("#customerContent").css("display", "none");
+$("#itemContent").css("display", "none");
+$("#orderContent").css("display", "none");
+
+$(".nav-item1").css("borderBottom", "2px solid gray")
+
+$(".nav-item1").click(function () {
+    $("#homeContent").css("display", "block");
+    $("#customerContent").css("display", "none");
+    $("#itemContent").css("display", "none");
+    $("#orderContent").css("display", "none");
+
+    $(".nav-item1").css("borderBottom", "2px solid gray")
+    $(".nav-item2").css("borderBottom", "none")
+    $(".nav-item3").css("borderBottom", "none")
+    $(".nav-item4").css("borderBottom", "none")
+});
+
+$(".nav-item2").click(function () {
+    $("#homeContent").css("display", "none");
+    $("#customerContent").css("display", "block");
+    $("#itemContent").css("display", "none");
+    $("#orderContent").css("display", "none");
+
+    $(".nav-item1").css("borderBottom", "none")
+    $(".nav-item2").css("borderBottom", "2px solid gray")
+    $(".nav-item3").css("borderBottom", "none")
+    $(".nav-item4").css("borderBottom", "none")
+
+    clearAllTexts();
+});
+
+$(".nav-item3").click(function () {
+    $("#homeContent").css("display", "none");
+    $("#customerContent").css("display", "none");
+    $("#itemContent").css("display", "block");
+    $("#orderContent").css("display", "none");
+
+    $(".nav-item1").css("borderBottom", "none")
+    $(".nav-item2").css("borderBottom", "none")
+    $(".nav-item3").css("borderBottom", "2px solid gray")
+    $(".nav-item4").css("borderBottom", "none")
+
+    itemClearAllTexts();
+});
+
+$(".nav-item4").click(function () {
+    $("#homeContent").css("display", "none");
+    $("#customerContent").css("display", "none");
+    $("#itemContent").css("display", "none");
+    $("#orderContent").css("display", "block");
+
+    $(".nav-item1").css("borderBottom", "none")
+    $(".nav-item2").css("borderBottom", "none")
+    $(".nav-item3").css("borderBottom", "none")
+    $(".nav-item4").css("borderBottom", "2px solid gray")
+
+    generateOrderId();
+    customerIdOption();
+    itemCodeOption();
+});
